f4e9559a64301c4e20b8ba168bc59f757ea6d859
Hoangnguyen72628 <96629210+Hoangnguyen72628@users.noreply.github.com>
2024-09-10 18:52:18 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,4 +5,4 @@ if ENV == "DEV" then
     loadstring(game:HttpGet(""))
 end]]
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/places/" .. game.GameId .. ".lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hoangnguyen72628/mspaint/main/places/" .. game.GameId .. ".lua))()
